047b637dc601b3d21a53c7dd968e2a6f066c0008
Tmalhussain <93242311+Tmalhussain@users.noreply.github.com>
2023-12-27 19:49:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import chess.polyglot
 import chess.pgn
 import chess.svg
 from search import *
+from evaluate import *
 import chess.syzygy
 
 player=input("Enter the color you want to play as (w/b): ")
@@ -15,13 +16,21 @@ board=chess.Board()
 
 while board.legal_moves.count()>0:
     if board.turn==ai_color:
-        result=nextMove(board,ai_color,0.1)
+        result=nextMove(board,ai_color)
         board.push(result)
         print(board)
     else:
         move=input("Enter your move: ")
-        while move not in board.legal_moves:
-            print("Invalid move")
-            move=input("Enter your move: ")
-        board.push_san(move)
+        while True:
+            try:
+                move=board.parse_san(move)
+                uci = move.uci()
+                break
+            except chess.InvalidMoveError:
+                print("Invalid move")
+                move=input("Enter your move: ")
+            except chess.IllegalMoveError:
+                print("Illegal move")
+                move=input("Enter your move: ")
+        board.push(move)
         print(board)
