60650e1021cbb11008081919e7661bb6ab449b0f
Tmalhussain <93242311+Tmalhussain@users.noreply.github.com>
2023-12-27 19:50:16 +0300

    Update search.py

--- search.py
+++ search.py
@@ -4,75 +4,75 @@ import time
 from evaluate import *
 
 max_depth = 18
-
-
-def nextMove(board, color, max_time):
+move_points = []
+def nextMove(board, color):
     try:
         best_move = chess.polyglot.MemoryMappedReader("books/human.bin").weighted_choice(board).move
-        return best_move
     except IndexError:
-        best_move = iterativeDeepening(board, color, max_time)
-        return best_move
+        best_move = iterativeDeepening(board, color,5)
+    return best_move
 
 
 def iterativeDeepening(board, color, max_time):
-    move_points = []
     start = time.time()
+    for move in board.legal_moves:
+        move_points.append((0,move))
+    best_move = None
     for i in range(1, max_depth + 1):
-        move_points = minimax(i, board, -9999, 9999, True, None, move_points)
-        move_points = orderMoves(move_points)
-        if time.time() - start > max_time:
+        points,best_move = minimax(i, board, float('inf'), -float('inf'), color==board.turn, None, color)
+        if time.time() - start >= max_time:
             break
-    return move_points[0][1]
+    move_points.clear()
+    return best_move
 
 
-def orderMoves(move_points):
-    move_points.sort(reverse=True)
-    return move_points
 
-
-def minimax(depth, board, alpha, beta, maxPlayer, curmove, move_points):
+def minimax(depth, board, alpha, beta, maxPlayer, curmove, ai_color):
     if depth == 0:
-        move_points.append((-eval(board), curmove))
-        if eval(board) == float('inf'):
+        for move in board.legal_moves:
+            if move == curmove:
+                points = quiescence(1, board, alpha, beta, maxPlayer)
+        quiescence(1, board, alpha, beta, maxPlayer)
+
+        if eval(board) == (float('inf') or -float('inf')) and board.turn == ai_color:
             print("mate you lose")
             exit(0)
-        if eval(board) == -float('inf'):
+        if eval(board) == (float('inf') or -float('inf')) and board.turn != ai_color:
             print("mate you win")
             exit(0)
         if eval(board) == "draw":
             print("draw")
             exit(0)
-        return -eval(board), move_points
+        return -eval(board), curmove
     if maxPlayer:
         maxEval = -float('inf')
         for move in board.legal_moves:
             board.push(move)
-            evalu, move_points = minimax(depth - 1, board, alpha, beta, False, move)
+            evalu,_= minimax(depth - 1, board, alpha, beta, False, move, ai_color)
             board.pop()
             maxEval = max(maxEval, evalu)
             alpha = max(alpha, evalu)
             if beta <= alpha:
                 break
-        return maxEval, move_points
+        return maxEval, curmove
     else:
         minEval = float('inf')
         for move in board.legal_moves:
             board.push(move)
-            evalu, move_points = minimax(depth - 1, board, alpha, beta, True, move)
+            evalu,_ = minimax(depth - 1, board, alpha, beta, True, move, ai_color)
             board.pop()
             minEval = min(minEval, evalu)
             beta = min(beta, evalu)
             if beta <= alpha:
                 break
-        return minEval, move_points
+        return minEval, curmove
 
 
 def quiescence(depth, board, alpha, beta, maximizingPlayer):
     if depth == 0:
         return -eval(board)
     if maximizingPlayer:
-        maxEval = -9999
+        maxEval = -float('inf')
         for move in board.legal_moves:
             if board.is_capture(move):
                 board.push(move)
@@ -84,7 +84,7 @@ def quiescence(depth, board, alpha, beta, maximizingPlayer):
                     break
         return maxEval
     else:
-        minEval = 9999
+        minEval = float('inf')
         for move in board.legal_moves:
             if board.is_capture(move):
                 board.push(move)
