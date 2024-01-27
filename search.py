c41745cc584dc135bfb4b49ec3be4424ee677337
Tmalhussain <93242311+Tmalhussain@users.noreply.github.com>
2024-01-27 21:02:21 +0300

    Update search.py

--- search.py
+++ search.py
@@ -1,77 +1,62 @@
-import chess
 import chess.polyglot
 import time
 from evaluate import *
 
 max_depth = 18
 move_points = []
-def nextMove(board, color):
+def nextMove(board: chess.Board, color: chess.Color)->(chess.Move):
     try:
         best_move = chess.polyglot.MemoryMappedReader("books/human.bin").weighted_choice(board).move
+        return best_move
     except IndexError:
-        best_move = iterativeDeepening(board, color,5)
+        best_move = iterativeDeepening(board, color, 5)
     return best_move
 
 
 def iterativeDeepening(board, color, max_time):
     start = time.time()
-    for move in board.legal_moves:
-        move_points.append((0,move))
     best_move = None
     for i in range(1, max_depth + 1):
-        points,best_move = minimax(i, board, float('inf'), -float('inf'), color==board.turn, None, color)
+        max_points = -float('inf')
+        points, best_move = minimax(i-1, board, -float('inf'), float('inf'), color == board.turn,None, not color)
+        print(f"iter at {i}: {best_move}: {points}")
         if time.time() - start >= max_time:
             break
-    move_points.clear()
+
     return best_move
-
-
-
-def minimax(depth, board, alpha, beta, maxPlayer, curmove, ai_color):
+def minimax(depth, board, alpha, beta, curPlayer, curmove, ai_color):
     if depth == 0:
-        for move in board.legal_moves:
-            if move == curmove:
-                points = quiescence(1, board, alpha, beta, maxPlayer)
-        quiescence(1, board, alpha, beta, maxPlayer)
+        return eval(board), curmove
+    if eval(board) == float('inf') or eval(board) == -float('inf'):
+        return eval(board), curmove
+    maxEval = float('inf') * (1 if curPlayer else -1)
+    bestmove = None
+    for move in board.legal_moves:
+        board.push(move)
+        evalu, searchmove = minimax(depth - 1, board, alpha, beta, not curPlayer, move, ai_color)
 
-        if eval(board) == (float('inf') or -float('inf')) and board.turn == ai_color:
-            print("mate you lose")
-            exit(0)
-        if eval(board) == (float('inf') or -float('inf')) and board.turn != ai_color:
-            print("mate you win")
-            exit(0)
-        if eval(board) == "draw":
-            print("draw")
-            exit(0)
-        return -eval(board), curmove
-    if maxPlayer:
-        maxEval = -float('inf')
-        for move in board.legal_moves:
-            board.push(move)
-            evalu,_= minimax(depth - 1, board, alpha, beta, False, move, ai_color)
-            board.pop()
-            maxEval = max(maxEval, evalu)
+        board.pop()
+        if curPlayer:
+            if evalu < maxEval:
+                maxEval = evalu
+                bestmove = move
+
             alpha = max(alpha, evalu)
-            if beta <= alpha:
-                break
-        return maxEval, curmove
-    else:
-        minEval = float('inf')
-        for move in board.legal_moves:
-            board.push(move)
-            evalu,_ = minimax(depth - 1, board, alpha, beta, True, move, ai_color)
-            board.pop()
-            minEval = min(minEval, evalu)
+        else:
+            if evalu > maxEval:
+                maxEval = evalu
+                bestmove = move
+
             beta = min(beta, evalu)
-            if beta <= alpha:
-                break
-        return minEval, curmove
 
 
-def quiescence(depth, board, alpha, beta, maximizingPlayer):
+        if beta <= alpha:
+            break
+    return maxEval, bestmove
+def quiescence(depth, board, alpha, beta, currentPlayer):
     if depth == 0:
         return -eval(board)
-    if maximizingPlayer:
+    if currentPlayer:
         maxEval = -float('inf')
         for move in board.legal_moves:
             if board.is_capture(move):
@@ -82,6 +67,8 @@ def quiescence(depth, board, alpha, beta, maximizingPlayer):
                 alpha = max(alpha, evalu)
                 if beta <= alpha:
                     break
+        if(maxEval==None):
+            return eval(board)
         return maxEval
     else:
         minEval = float('inf')
